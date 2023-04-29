57c6ab0d6cc57727813adb0aab231005cf3f9d45
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2023-04-29 15:39:54 +0900

    add script generates VisualAtom_dataset

---
+++ run.jl
@@ -0,0 +1,32 @@
+using Distributed: @distributed, @everywhere, nprocs
+using TOML
+
+using VisualAtom: Config
+using VisualAtom: generate_instances
+using ToStruct: tostruct
+using ProgressMeter: @showprogress
+
+@everywhere using VisualAtom: generate_instances
+
+function generate_dataset(
+    config_path::AbstractString,
+    save_root::AbstractString,
+)
+    @info "Read configuration:" config_path
+    config = tostruct(Config, TOML.parsefile(config_path))
+
+    num_categories = config.num_categories
+    num_instances = config.num_instances
+
+    @info "Generate VisualAtom_dataset" num_categories num_instances nprocs()
+    @showprogress @distributed for category_id in 0:(num_categories-1)
+        generate_instances(config; save_root, category_id, num_instances)
+    end
+    println("Done! checkout $(save_root)")
+end
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    config_path = "config.toml"
+    save_root = "VisualAtom_dataset"
+    @time generate_dataset(config_path, save_root)
+end
